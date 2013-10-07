9323f669d0cf0f6f5c87e2981201776ed5ae9bcd
Yonne de Bruijn <yonnedebruijng@gmail.com>
2013-10-07 13:43:16 +0200

    Test

--- Parameters.java
+++ Parameters.java
@@ -1,6 +1,6 @@
 public class Parameters {
     // algorithm parameters
-    public double mutation_rate = 0.05;
+    public double mutation_rate = 0.1;
 
     // data parameters
     public boolean isMultimodal = false;     // if the data has several peaks in the distribution
