c4401840de0a39b5c79414ee5497881b0d259a5c
Yonne de Bruijn <yonnedebruijng@gmail.com>
2013-10-17 19:28:28 +0200

    Bug Fixes [NOT TESTED]
    - fix out of bounds, start at j = 1, instead of j+1 --> j-1
    - restructured loops
    - implemented comments

--- Player13.java
+++ Player13.java
@@ -19,8 +19,8 @@ public class Player13 implements ContestSubmission{
 
     private int eval_limit;     // amount of allowed tries on the server
     private int counter = 0;    // counter that represents the amount of evaluated tries
-    final private int parent_size = (int) p.parent_portion * p.population_size;
-    final private int elite_size = (int) p.elite_portion * p.population_size;
+    final private int parent_size = (int) (p.parent_portion * p.population_size);
+    final private int elite_size = (int) (p.elite_portion * p.population_size);
 
     public Player13 ()
     {
@@ -28,7 +28,7 @@ public class Player13 implements ContestSubmission{
         //Create the populations of 'individuals'
         population = new ArrayList<Individual>(p.population_size);
         parent_population = new ArrayList<Individual>(parent_size);
-        child_population = new ArrayList<Individual>();
+        child_population = new ArrayList<Individual>(2*parent_size);
 
         for(int i = 0; i < p.population_size; i++)
         {
@@ -81,34 +81,38 @@ public class Player13 implements ContestSubmission{
     {
         //Stay within the amount of tries set by the server
         System.out.println("Start Population");
-        while(counter < p.population_size)
+
+        while(counter < eval_limit)
         {
-            double fitness = (Double)evaluation.evaluate(population.get(counter).getVector());
-            population.get(counter).setFitness(fitness);
-            Collections.sort(population);
-            System.out.println(population.get(counter).getFitness());
-            counter++;
-        }
+            for(int g = 0; g < p.population_size; g++)
+            {
+                double fitness = (Double)evaluation.evaluate(population.get(g).getVector());
+                population.get(g).setFitness(fitness);
+                Collections.sort(population);
+                System.out.println(population.get(g).getFitness());
+                counter++;
+            }
 
         //Create the parent population
         for(int i = 0; i < parent_size; i++)
         {
             parent_population.add(population.get(i));
-            Collections.sort(parent_population);
         }
+        Collections.sort(parent_population);
 
         System.out.println("Child Population");
         //Create the child population by applying crossover on the parent population
-        for(int j = 0; j < parent_population.size(); j++)
+        for(int j = 1; j < parent_population.size(); j++)
         {
-            child_population.add(parent_population.get(j).crossover(parent_population.get(j+1),p));
-            child_population.add(parent_population.get(j+1).crossover(parent_population.get(j),p));
+            child_population.add(parent_population.get(j).crossover(parent_population.get(j-1),p));
+            child_population.add(parent_population.get(j-1).crossover(parent_population.get(j),p));
             Individual evaluation_child = child_population.get(j);
             double fitness = (Double)evaluation.evaluate(evaluation_child.getVector());
             evaluation_child.setFitness(fitness);
             counter++;
-            System.out.println(child_population.get(counter).getFitness());
+            System.out.println(child_population.get(j).getFitness());
         }
-            Collections.sort(child_population);
+        Collections.sort(child_population);
     }
+}
 }
